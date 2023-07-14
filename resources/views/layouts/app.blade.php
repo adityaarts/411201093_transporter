bd1e9f447924cbc1fc15d279ca34b224c14c369f
anonymous <anonymous@anonymous.com>
2023-07-14 20:55:10 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -162,7 +162,7 @@
 
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-      <strong>Copyright &copy; 2022</strong>
+      <strong>Copyright &copy; 2022 Hariyanto Aditya Ramadhan 411201093</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
